8cb98684834c5b190490b690334d3f6525791a2f
Matheus Dornelles <matheus.dorne@gmail.com>
2024-08-13 14:25:05 -0300

    up

--- sync-screen.php
+++ sync-screen.php
@@ -7,7 +7,18 @@ echo $anoLancamento."\n";
 if ($anoLancamento > 2022) {
  echo "Esse filme é um lancaçmento\n";
 } else {
-    echo "Esse filme não é um lançamento";
+    echo "Esse filme não é um lançamento\n";
 }
 
-echo "teste";
+
+$nomeFilme = "Filme 2";
+$nomeFilme = "Filme 1";
+
+$genero = match ($nomeFilme) {
+    "Filme 1" => "Ruim\n",
+    "Filme 2" => "Musica\n",
+    default => "gênero desconhecido",
+
+};
+
+echo $genero;
